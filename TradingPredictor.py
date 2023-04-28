b8e540e69091a75b17868cd64d61636c1fd4e384
Felix Ben-Eghan <fbe2012@hotmail.co.uk>
2023-04-28 07:01:00 +0100

    Update and rename GasTradingPredictor.py to TradingPredictor.py

--- TradingPredictor.py
+++ TradingPredictor.py
@@ -7,18 +7,18 @@ from sklearn.model_selection import GridSearchCV, cross_val_score
 from sklearn.metrics import mean_squared_error
 
 
-class GasTradingPredictor:
+class TradingPredictor:
     """
-    A class for predicting gas trading performance using ESG metrics.
+    A class for predicting trading performance using ESG metrics.
     """
 
     def __init__(self, db_file, table_name, numerical_features, categorical_features, target):
         """
-        Initializes a GasTradingPredictor object.
+        Initializes a TradingPredictor object.
 
         Parameters:
-            db_file (str): The name of the database file containing the gas trading data.
-            table_name (str): The name of the table containing the gas trading data.
+            db_file (str): The name of the database file containing the trading data.
+            table_name (str): The name of the table containing the trading data.
             numerical_features (list of str): The names of the numerical features in the data.
             categorical_features (list of str): The names of the categorical features in the data.
             target (str): The name of the target variable in the data.
@@ -44,7 +44,7 @@ class GasTradingPredictor:
 
     def clean_data(self):
         """
-        Cleans the gas trading data.
+        Cleans the trading data.
         """
         self.analyzer.clean_data()
 
@@ -85,14 +85,14 @@ class GasTradingPredictor:
         
 if __name__ == '__main__':
     # Set up database and input variables
-    db_file = 'gas_trading_data.db'
-    table_name = 'gas_trading_table'
+    db_file = 'trading_data.db'
+    table_name = 'trading_table'
     numerical_features = ['esg_metric_1', 'esg_metric_2', 'non_esg_metric_1', 'non_esg_metric_2']
     categorical_features = ['region', 'industry']
-    target = 'gas_trading_performance'
+    target = 'trading_performance'
 
     # Create predictor object and clean the data
-    predictor = GasTradingPredictor(db_file, table_name, numerical_features, categorical_features, target)
+    predictor = TradingPredictor(db_file, table_name, numerical_features, categorical_features, target)
     predictor.clean_data()
 
     # Set up parameter grid for grid search
